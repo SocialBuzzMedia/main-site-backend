84256b43ce33524e5e21c924d1a7167d4b06b29e
Social Buzz Media <hellosocialbuzzmedia@gmail.com>
2024-09-27 18:35:28 +0530

    nothing changes

--- server.js
+++ server.js
@@ -52,6 +52,6 @@ app.use(errorResponseHandler);
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is running on port ${PORT}`);
 });
